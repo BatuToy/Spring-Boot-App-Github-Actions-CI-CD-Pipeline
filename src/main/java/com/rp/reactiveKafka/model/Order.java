7dbf792c1e5f44343fb280f7f03c9a66b62b5d18
BatuToy <batu_toy@hotmail.com>
2024-10-06 20:32:48 +0300

    .dockerignore file added

--- src/main/java/com/rp/reactiveKafka/model/Order.java
+++ src/main/java/com/rp/reactiveKafka/model/Order.java
@@ -18,7 +18,7 @@ import java.util.Date;
 @Table(name =  "t_orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     @Column(name = "ORDER_NAME")
     private String orderName;
